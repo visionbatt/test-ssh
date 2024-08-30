45badfba55b56ea0655ca7bc0e1ca86a737e4d74
visionbatt <153471885+visionbatt@users.noreply.github.com>
2024-08-30 13:53:35 +0800

    Update restore_vless.py

--- restore_vless.py
+++ restore_vless.py
@@ -2,52 +2,45 @@ import os
 import json
 import paramiko
 
-# 从环境变量中获取 ACCOUNTS_JSON
+# 获取环境变量中的账户信息
 accounts_json = os.getenv('ACCOUNTS_JSON')
 
+# 尝试加载 JSON 数据
 try:
     servers = json.loads(accounts_json)
 except json.JSONDecodeError:
-    error_message = "ACCOUNTS_JSON 参数格式错误"
-    print(error_message)
+    print("ACCOUNTS_JSON 参数格式错误")
     exit(1)
 
 summary_message = "serv00-vless 恢复操作结果：\n"
 default_restore_command = "cd ~/domains/$USER.serv00.net/vless && ./check_vless.sh"
 
-# 使用 paramiko 进行 SSH 连接和执行命令
+# 遍历每个服务器信息
 for server in servers:
     host = server['host']
     port = server['port']
     username = server['username']
     password = server['password']
     cron_command = server.get('cron', default_restore_command)
-    
-    print(f"连接到 {host}...")
 
-    # 创建 SSH 客户端
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print(f"连接到 {host}...")
     
+    # 使用 Paramiko 进行 SSH 连接
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 自动添加主机密钥
+
     try:
-        # 连接到服务器
-        client.connect(hostname=host, port=port, username=username, password=password)
-        
-        # 执行命令
-        stdin, stdout, stderr = client.exec_command(cron_command)
+        ssh.connect(hostname=host, port=port, username=username, password=password)
+        stdin, stdout, stderr = ssh.exec_command(cron_command)
         output = stdout.read().decode('utf-8')
         error = stderr.read().decode('utf-8')
-        
         if output:
             summary_message += f"\n成功恢复 {host} 上的 vless 服务：\n{output}"
         if error:
-            summary_message += f"\n{host} 上的 vless 服务恢复时出错：\n{error}"
-    
+            summary_message += f"\n恢复 {host} 上的 vless 服务时出错：\n{error}"
     except paramiko.SSHException as e:
-        summary_message += f"\n无法连接到 {host}：{str(e)}"
-    
+        summary_message += f"\n无法恢复 {host} 上的 vless 服务：\n{str(e)}"
     finally:
-        # 关闭连接
-        client.close()
+        ssh.close()
 
 print(summary_message)
